c1b196a06bf6ff98add1ffc7cbf7099170d2f726
nonsense9 <67061900+nonsense9@users.noreply.github.com>
2020-09-15 13:01:57 +0300

    Introduction_Basics

---
+++ app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  now: Date = new Date()
+
+
+
+}
+  // Toggle:any = false;
+
+//   arr = [1, 1, 2, 3, 7, 13, 15];
+
+//   objects = [
+//     {title: 'Angular', author: 'by Goose', comments: [
+//       {name: 'Name', text: 'text1'},
+//       {name: 'Name', text: 'text2'},
+//       {name: 'Name', text: 'text3'},
+//     ]},
+//     {title: 'NorAngular', author: 'by Guess', comments: [
+//       {name: 'Name2', text: 'text1'},
+//       {name: 'Name2', text: 'text2'},
+//       {name: 'Name2', text: 'text3'},
+//     ]},
+//   ]
+
